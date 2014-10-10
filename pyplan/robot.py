06357ead32c06b4f593eb693e5d28acd520bb57d
Clement Gehring <gehring@mit.edu>
2014-10-10 13:01:09 -0400

    debugging rrts

--- pyplan/robot.py
+++ pyplan/robot.py
@@ -35,9 +35,7 @@ class Point_Robot(Robot):
         return pos
 
     def sample_config(self, pos1, pos2, nsamples):
-        print pos1
-        print pos2
-        return np.linspace(pos1, pos2, nsamples)
+        return np.array([np.linspace(i, j, nsamples) for i,j in zip(pos1, pos2)])
 
 class Poly_Robot(Robot):
     angle_ratio = 0.1
